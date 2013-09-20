58c5b3a2aaab8a046b5c7b1025c2f640a10453f8
Hank Wang <drapho@gmail.com>
2013-09-20 09:23:53 +0800

    sample to set data to jade

--- day2/routes/index.js
+++ day2/routes/index.js
@@ -4,13 +4,16 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', {});
+    res.render('index', {
+        name: 'Hank Wang',
+        hash: 'ab7613f4699348155cb6ca555c42508d'
+    });
 };
 
 exports.contact = function(req, res){
-  res.render('contact', {});
+    res.render('contact', {});
 };
 
 exports.about = function(req, res){
-  res.render('about', {});
+    res.render('about', {});
 };
